51db5eb289cb1f94680c2532fac6ef29529fcad9
James Taco Pozdena <jpoz@simple.com>
2013-10-30 16:52:29 -0700

    spliting on non carriage return line breaks too

--- util.go
+++ util.go
@@ -9,6 +9,9 @@ func splitEML(eml []byte) (header, body []byte, err error) {
 	if c := bytes.SplitN(eml, []byte("\r\n\r\n"), 2); len(c) == 2 {
 		return c[0], c[1], nil
 	}
+	if c := bytes.SplitN(eml, []byte("\n\n"), 2); len(c) == 2 {
+		return c[0], c[1], nil
+	}
 	err = errors.New("could not read header block")
 	return
 }
--- util_test.go
+++ util_test.go
@@ -1,33 +1,42 @@
 package dkim
 
 import (
+	"fmt"
 	"testing"
 )
 
-var utilSampleEML []byte = []byte("A: X\r\n" +
-	"B : Y\t\r\n" +
-	"\tZ  \r\n" +
-	"\r\n" +
-	" C \r\n" +
-	"D \t E\r\n" +
-	"\r\n" +
-	"\r\n")
+var utilSampleEML1 [][]byte = [][]byte{
+	[]byte("A: X\r\n" + "B : Y\t\r\n" + "\tZ  \r\n" + "\r\n" + " C \r\n" + "D \t E\r\n" + "\r\n" + "\r\n"),
+	[]byte("A: X\r\nB : Y\t\r\n\tZ  "),
+	[]byte(" C \r\nD \t E\r\n\r\n\r\n"),
+}
+
+var utilSampleEML2 [][]byte = [][]byte{
+	[]byte("A: X\n" + "B : Y\t\n" + "\tZ  \n" + "\n" + " C \n" + "D \t E\n" + "\n" + "\n"),
+	[]byte("A: X\nB : Y\t\n\tZ  "),
+	[]byte(" C \nD \t E\n\n\n"),
+}
+
+var utilSampleEMLs [][][]byte = [][][]byte{utilSampleEML1, utilSampleEML2}
 
 func TestSplitEML(t *testing.T) {
-	header, body, err := splitEML(utilSampleEML)
-	if err != nil {
-		t.Fatal("error not nil", err)
-	}
-	if len(header) == 0 {
-		t.Fatal("wrong header length", len(header))
-	}
-	if len(body) == 0 {
-		t.Fatal("wrong body length", len(body))
-	}
-	if x := string(header); x != "A: X\r\nB : Y\t\r\n\tZ  " {
-		t.Fatal("wrong header", x)
-	}
-	if x := string(body); x != " C \r\nD \t E\r\n\r\n\r\n" {
-		t.Fatal("wrong body", x)
+	for _, utilSampleEMLparts := range utilSampleEMLs {
+		utilSampleEML := utilSampleEMLparts[0]
+		header, body, err := splitEML(utilSampleEML)
+		if err != nil {
+			t.Fatal("error not nil", err)
+		}
+		if len(header) == 0 {
+			t.Fatal("wrong header length", len(header))
+		}
+		if len(body) == 0 {
+			t.Fatal("wrong body length", len(body))
+		}
+		if x := string(header); x != string(utilSampleEMLparts[1]) {
+			t.Fatal(fmt.Sprintf("wrong header: %q should be %q", x, string(utilSampleEMLparts[1])))
+		}
+		if x := string(body); x != string(utilSampleEMLparts[2]) {
+			t.Fatal(fmt.Sprintf("wrong body: %q should be %q", x, string(utilSampleEMLparts[2])))
+		}
 	}
 }

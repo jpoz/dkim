3f08fd71519877c83009fb7f6c614cfe290cc2e3
James Taco Pozdena <jpoz@simple.com>
2013-10-31 15:34:47 -0700

    first pass at using net/mail to parse email message

--- util.go
+++ util.go
@@ -2,25 +2,10 @@ package dkim
 
 import (
 	"bytes"
-	"fmt"
 	"net/mail"
 )
 
-func splitEML(eml []byte) (header, body []byte, err error) {
+func readEML(eml []byte) (m *mail.Message, err error) {
 	r := bytes.NewReader(eml)
-	msg, err := mail.ReadMessage(r)
-	if err != nil {
-		return header, body, err
-	}
-
-	header_buf := bytes.NewBuffer([]byte{})
-	for k, v := range msg.Header {
-		s := fmt.Sprintf("%s: %s\n", k, v[0])
-		header_buf.Write([]byte(s))
-	}
-
-	body_buf := new(bytes.Buffer)
-	body_buf.ReadFrom(msg.Body)
-
-	return header_buf.Bytes(), body_buf.Bytes(), nil
+	return mail.ReadMessage(r)
 }
